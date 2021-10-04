443f619d6455c38e3e94ca4ba644f7f3eb1574fb
apoorvshrimali <88945648+apoorvshrimali@users.noreply.github.com>
2021-10-04 21:46:51 +0530

    bracket arrangement

--- hypotenuse.js
+++ hypotenuse.js
@@ -5,7 +5,7 @@ const outputDiv = document.querySelector("#output");
 
 function calculateHypotenuse() {
 
-    const storeSum = Number(inputs[0].value) * (Number(inputs[0].value) + Number(inputs[1].value)) * Number(inputs[1].value);
+    const storeSum = (Number(inputs[0].value) * Number(inputs[0].value)) + (Number(inputs[1].value) * Number(inputs[1].value));
 
     const lengthOfHypotenuse = Math.sqrt(storeSum).toFixed(2);
 
